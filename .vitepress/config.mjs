3b3a4c49f528d81bffddd622e67053031f24cfe9
dushouxin.dsx <dushouxin.dsx@alibaba-inc.com>
2024-06-20 00:28:24 +0800

    init

---
+++ .vitepress/config.mjs
@@ -0,0 +1,40 @@
+import { defineConfig } from 'vitepress'
+
+// https://vitepress.dev/reference/site-config
+export default defineConfig({
+  title: "My Awesome Project",
+  description: "A VitePress Site",
+  themeConfig: {
+    // https://vitepress.dev/reference/default-theme-config
+    nav: [
+      { text: 'Home', link: '/example/' },
+      { text: 'Docs', link: '/docs' },
+      { text: 'Examples', link: '/example/markdown-examples' }
+    ],
+
+    sidebar: {
+      '/example/': [
+        {
+          text: 'Examples',
+          items: [
+            { text: 'Markdown Examples', link: '/example/markdown-examples' },
+            { text: 'Runtime API Examples', link: '/example/api-examples' }
+          ]
+        }
+      ],
+      '/docs/':[
+        {
+          text: 'Docs',
+          items: [
+            { text: 'home', link: '/docs/index' },
+            { text: 'demo', link: '/docs/demo' }
+          ]
+        }
+      ]
+    },
+
+    socialLinks: [
+      { icon: 'github', link: 'https://github.com/vuejs/vitepress' }
+    ]
+  }
+})
---
+++ docs/demo.md
@@ -0,0 +1,5 @@
+### **源文件**
+
+`.vitepress` 目录之外的 Markdown 文件被视为**源文件**。
+
+VitePress 使用 **基于文件的路由**：每个 `.md` 文件将在相同的路径被编译成为 `.html` 文件。例如，`index.md` 将会被编译成 `index.html`，可以在生成的 VitePress 站点的根路径 `/` 进行访问。
